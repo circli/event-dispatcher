749a22da6d03eb0a95eec930a33bd15bfd4d14b9
Andreas Sundqvist <andreas@stefna.is>
2020-09-03 16:54:55 +0200

    Don't support php8 until fig/event-dispatcher-util is updated

--- tests/EventDispatcherAwareTraitTest.php
+++ tests/EventDispatcherAwareTraitTest.php
@@ -5,6 +5,7 @@ namespace Tests;
 use Circli\EventDispatcher\EventDispatcher;
 use Circli\EventDispatcher\EventDispatcherAwareTrait;
 use Circli\EventDispatcher\NullEventDispatcher;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\EventDispatcher\ListenerProviderInterface;
 
@@ -12,19 +13,21 @@ class EventDispatcherAwareTraitTest extends TestCase
 {
     public function testSetDispatcher()
     {
+        /** @var MockObject|EventDispatcherAwareTrait $trait */
         $trait = $this->getMockForTrait(EventDispatcherAwareTrait::class);
-
         $this->assertSame($trait, $trait->setEventDispatcher(new NullEventDispatcher()));
     }
 
     public function testGetDefaultDispatcher()
     {
+        /** @var MockObject|EventDispatcherAwareTrait $trait */
         $trait = $this->getMockForTrait(EventDispatcherAwareTrait::class);
         $this->assertInstanceOf(NullEventDispatcher::class, $trait->getEventDispatcher());
     }
 
     public function testGetSetDispatcher()
     {
+        /** @var MockObject|EventDispatcherAwareTrait $trait */
         $trait = $this->getMockForTrait(EventDispatcherAwareTrait::class);
 
         $dispatcher = new EventDispatcher($this->createMock(ListenerProviderInterface::class));
