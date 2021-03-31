d64f5bcff7a08d1a3c89f0b981714541ef1a5826
Andreas Sundqvist <andreas@stefna.is>
2021-03-31 10:33:33 +0200

    Add toggle provider to be able to disable providers

---
+++ src/ListenerProvider/ToggleProvider.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace Circli\EventDispatcher\ListenerProvider;
+
+use Psr\EventDispatcher\ListenerProviderInterface;
+
+final class ToggleProvider implements ListenerProviderInterface
+{
+    /** @var ListenerProviderInterface */
+    private $provider;
+    /** @var bool */
+    private $active = true;
+
+    public function __construct(ListenerProviderInterface $provider)
+    {
+        $this->provider = $provider;
+    }
+
+    public function disable(): void
+    {
+        $this->active = false;
+    }
+
+    public function enable(): void
+    {
+        $this->active = true;
+    }
+
+    /**
+     * @return iterable<callable>
+     */
+    public function getListenersForEvent(object $event): iterable
+    {
+        if ($this->active === false) {
+            return [];
+        }
+        yield from $this->provider->getListenersForEvent($event);
+    }
+}
